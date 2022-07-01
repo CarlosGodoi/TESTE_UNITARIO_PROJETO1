37506414d1e435d62e4d3ad43c507acc212a177b
Carlos Eduardo <61329097+CarlosGodoi@users.noreply.github.com>
2022-07-01 15:52:06 -0300

    Testes simulando ações em carrinho de compras.

---
+++ src/lib/Cart.spec.js
@@ -0,0 +1,73 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
+
+  let product2 = {
+    title: 'Adidas running shoes - men',
+    price: 41872,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created intance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply quantity and price e receive the total amount', () => {});
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+      // 112.648
+    });
+  });
+
+  describe('checkout', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+  });
+});
