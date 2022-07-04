6886b6b5554bf22f72f1dd47bcee5fae6962e108
Carlos Eduardo <61329097+CarlosGodoi@users.noreply.github.com>
2022-07-04 09:24:24 -0300

    Implementando teste para condição de desconto

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -18,7 +18,7 @@ describe('Cart', () => {
 
   describe('getTotal()', () => {
     it('should return 0 when getTotal() is executed in a newly created intance', () => {
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it('should multiply quantity and price e receive the total amount', () => {});
@@ -34,7 +34,7 @@ describe('Cart', () => {
         quantity: 1,
       });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it('should update total when a product gets included and then removed', () => {
@@ -50,7 +50,7 @@ describe('Cart', () => {
 
       cart.remove(product);
 
-      expect(cart.getTotal()).toEqual(41872);
+      expect(cart.getTotal().getAmount()).toEqual(41872);
       // 112.648
     });
   });
@@ -68,6 +68,34 @@ describe('Cart', () => {
       });
 
       expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    describe('checkout', () => {
+      it('should return an object with the total and the list of items when summary() is called', () => {
+        cart.add({
+          product,
+          quantity: 2,
+        });
+
+        cart.add({
+          product: product2,
+          quantity: 3,
+        });
+
+        expect(cart.summary()).toMatchSnapshot();
+        expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+      });
+
+      it('should reset the when checkout() is called', () => {
+        cart.add({
+          product: product2,
+          quantity: 3,
+        });
+
+        cart.checkout();
+
+        expect(cart.getTotal().getAmount()).toEqual(0);
+      });
     });
   });
 });
